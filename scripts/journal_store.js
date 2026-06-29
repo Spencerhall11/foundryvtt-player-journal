794dd75413a27747ae1d1487742526df81270669
shall053 <shall053@odu.edu>
2026-06-29 04:15:04 -0400

    added journal functions

---
+++ scripts/journal_store.js
@@ -0,0 +1,64 @@
+
+export function getPersonalJournal(){
+    
+    const all = game.settings.get("foundryvtt-player-journal", "personal-journals");
+    
+    return all[game.user.id] ?? { entries: [] };
+}
+
+export async function savePersonalJournal(journal){
+    const all = game.settings.get("foundryvtt-player-journal", "personal-journals");
+    
+    all[game.user.id] = journal;
+    
+    await game.settings.set("foundryvtt-player-journal", "personal-journals", all);
+}
+
+export function getPartyJournal(){
+    
+    const all = game.settings.get("foundryvtt-player-journal", "party-journals");
+    
+    return all ?? { entries: [] };
+}
+
+export async function savePartyJournal(journal){
+    const all = game.settings.get("foundryvtt-player-journal", "party-journals");
+
+    all.entries = journal;
+        
+    await game.settings.set("foundryvtt-player-journal", "party-journals", all);
+}
+
+export function getGroupJournal(groupId) {
+
+    const all = game.settings.get("foundryvtt-player-journal", "group-journals");
+
+    return all[groupId] ?? { entries: [] };
+}
+
+export async function saveGroupJournal(groupId, journal) {
+
+    const all = game.settings.get("foundryvtt-player-journal", "group-journals");
+
+    all[groupId] = journal;
+
+    await game.settings.set("foundryvtt-player-journal", "group-journals", all);
+}
+
+export function getCodex() {
+
+    const all = game.settings.get("foundryvtt-player-journal", "player-codex");
+
+    return all[game.user.id] ?? { items: [], bestiary: [] };
+
+}
+
+export async function saveCodex(codex) {
+
+    const all = game.settings.get("foundryvtt-player-journal", "player-codex");
+
+    all[game.user.id] = codex;
+
+    await game.settings.set("foundryvtt-player-journal", "player-codex", all);
+    
+}
